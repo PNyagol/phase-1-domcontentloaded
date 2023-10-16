a4b79d406aa2ed6b00c2132e66c19ee4059039ba
Nyagol <nyagolpeter@gmail.com>
2023-10-16 07:50:00 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    let paragraph = document.getElementById("text");
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    } else {
+      console.error("The paragraph element with id 'text' was not found.");
+    }
+  });
+  
